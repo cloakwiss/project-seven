53c3441f6c78be53edbaadf363db5d7359fc8c1a
AdiBahe <adityabahe1111@gmail.com>
2026-04-15 11:48:50 +0530

    relative bath in testing bxild.bat

--- bxild.bat
+++ bxild.bat
@@ -5,12 +5,12 @@ setlocal enabledelayedexpansion
 :: Global settings
 :: -------------------------------
 set CFLAGS=-nologo -D_AMD64_ -EHsc
-
-
+set CUR_DIR=%CD%
+echo %CUR_DIR%
 pushd builds\debug\
 cl %CFLAGS% -Zi -LD ..\..\hookdll\hook.cpp -link ..\..\builds\debug\detours\detours.lib
 popd
 
-builds\debug\main.exe -e:.\builds\debug\samples\MessageBox.exe -d:E:\tmp\proj-mini\p7\builds\debug\hook.dll
+builds\debug\main.exe -e:.\builds\debug\samples\MessageBox.exe -d:%CUR_DIR%\builds\debug\hook.dll
 
 raddbg  --project:"hook.rad" builds\debug\samples\MessageBox.exe -q
